3ec9de467db229535324f42228688abce3cb257d
SylvainLap <sylvain.lapendry@gmail.com>
2016-07-12 11:28:33 +0200

    add logs to controller

--- app/controllers/index_controller.js
+++ app/controllers/index_controller.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const Nodal = require('nodal');
+const os = require('os');
 
 class IndexController extends Nodal.Controller {
 
   get() {
+
+    console.log('begin');
+    console.log(os.cpus().length);
+    console.log(process.env.WEB_CONCURRENCY);
+    console.log('end');
 
     this.respond({message: 'Welcome to your Nodal Project'});
 
